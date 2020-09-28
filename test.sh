4899ce0d4d7eb9661008442b73a12395c918f604
Christoph Heinen <heinen.fsinfo@uni-bonn.de>
2020-09-28 12:22:40 +0200

    testpfade angepasst

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-sudo PYTHONPATH=".:rpi_ws281x/build/lib.linux-armv7l-2.7" python3 rpi_ws281x/examples/strandtest.py -c
+sudo PYTHONPATH=".:rpy_ws281x/python:rpi_ws281x/build/lib.linux-armv7l-2.7" python3 rpi_ws281x/python/examples/strandtest.py -c
