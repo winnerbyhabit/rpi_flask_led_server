e16e779dd19561f62b3edadb8a9ee89243650813
Christoph Heinen <heinen.fsinfo@uni-bonn.de>
2020-09-28 20:25:39 +0200

    rainbow

--- led_functions.py
+++ led_functions.py
@@ -27,7 +27,7 @@ def set_color(red,blue,green):
 
 def set_rainbow_color():
     for i in range(the_strip.numPixels()):
-        the_strip.setPixelColor(i, wheel((i+j) & 255))
+        the_strip.setPixelColor(i, wheel(i & 255))
     the_strip.show()
 
 # Define functions which animate LEDs in various ways.
