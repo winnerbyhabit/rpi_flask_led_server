9b58009d30eefa431d44e180bca7709ee15fdbad
Christoph Heinen <heinen.fsinfo@uni-bonn.de>
2020-09-28 22:10:02 +0200

    rainbow

--- start_server.py
+++ start_server.py
@@ -9,7 +9,7 @@ from led_functions import set_color,init,set_rainbow_color
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('buttonpage.html', functions=['clear','rainbow','all_green','all_red','all_blue','all_lila'])
+    return render_template('buttonpage.html', functions=['clear','rainbow','all_green','all_red','all_blue','all_pink','all_white'])
     
 @app.route('/', methods=['POST'])
 def parse_request():
@@ -24,10 +24,12 @@ def parse_request():
         set_color(255,0,0)
     elif function == 'all_green':
         set_color(0,0,255)
-    elif function == 'all_lila':
+    elif function == 'all_pink':
         set_color(127,127,0)
     elif function == 'rainbow':
         set_rainbow_color()
+    elif function == 'all_white':
+        set_color(127,127,127)
     return index()
     
 if __name__ == '__main__':
