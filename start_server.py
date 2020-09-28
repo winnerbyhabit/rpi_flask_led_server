7aec627a732a36e183fd02aefe6c030b7aadcbd6
Christoph Heinen <heinen.fsinfo@uni-bonn.de>
2020-09-28 18:00:32 +0200

    test webserver

---
+++ start_server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from flask import Flask,render_template,request
+
+import config
+
+from led_functions import *
+
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('buttonpage.html', functions=['clear','rainbow','all_green'])
+    
+@app.route('/', methods=['POST'])
+def parse_request():
+    data = request.form
+    function = data.get('function')
+    
+    if function == 'clear':
+        colorWipe(strip, Color(0,0,0), 10)
+    return index()
+    
+if __name__ == '__main__':
+    app.run(debug=True, port=80, host='0.0.0.0')
