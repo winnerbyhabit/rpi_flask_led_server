e600ffbf59e7a446ea6efc724a0c61b81d465e97
Christoph Heinen <heinen.fsinfo@uni-bonn.de>
2021-03-31 15:02:44 +0200

    fehlende software installieren, versionsupdate

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,1 @@
-sudo PYTHONPATH="$PYTHONPATH:.:rpy_ws281x/python:rpi_ws281x/build/lib.linux-armv7l-2.7" python3 start_server.py
+sudo PYTHONPATH="$PYTHONPATH:.:rpy_ws281x/python:rpi_ws281x/build/lib.linux-armv7l-3.7" python3 start_server.py
