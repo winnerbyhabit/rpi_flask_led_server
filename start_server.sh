f352fef262adb0900e7d026c982c8e95520fba92
Christoph Heinen <heinen.fsinfo@uni-bonn.de>
2021-03-31 15:13:47 +0200

    bugfix

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,1 @@
-sudo PYTHONPATH="$PYTHONPATH:.:rpy_ws281x/python:rpi_ws281x/build/lib.linux-armv7l-3.7" python3 start_server.py
+sudo PYTHONPATH="$PYTHONPATH:.:rpi_ws281x/python:rpi_ws281x/python/build/lib.linux-armv6l-3.7" python3 start_server.py
